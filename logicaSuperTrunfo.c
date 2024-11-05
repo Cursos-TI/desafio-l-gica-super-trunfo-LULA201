5fee2f280ea0edb4a146a2b8940b4b1821301970
LULA201 <fakam22@gmail.com>
2024-11-05 20:30:47 +0000

    Adicionando dados de cidades no Super Trunfo

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -6,7 +6,355 @@
 // Siga os comentários para implementar cada parte do desafio.
 
 int main() {
-    // Definição das variáveis para armazenar as propriedades das cidades
+        // Definição das variáveis para armazenar as propriedades das cidades
+Estado: A
+Codigo da Carta:A01
+Nome da Cidade: Rio de Janeiro
+População: 6748000 (estimativa 2023)
+Área: 1.221 km²
+PIB em reais: R$ 409,7 bilhões (IBGE 2020)
+Número de pontos turísticos: 96 pontos principais (estimativa, considerando atrações icônicas)
+
+codigo da carta:A02
+nome da cidade:Niteroi
+População: 520.563 habitantes (estimativa 2023)
+Área: 133,9 km²
+PIB em reais: R$ 24,7 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 15 principais pontos turísticos
+
+
+codigo da carta :A03
+nome da cidade:Petropolis
+População: 310.102 habitantes (estimativa 2023)
+Área: 795,8 km²
+PIB em reais: R$ 11,9 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 20 principais pontos turísticos
+
+
+codigo da carta:A04
+nome da cidade:angra dos reis
+População: 167.434 habitantes (Censo 2022) 
+IBGE
+Área: 813,420 km² 
+IBGE
+PIB em reais: R$ 11194244230,00 (2021) 
+CIDADES IBGE
+Número de pontos turísticos:aproximadamente 30 pontos turisticos
+
+
+
+
+
+Estado:B 
+Codigo da Carta:B01
+Nome da Cidade: Porto Alegre
+População: 1332845 habitantes (Censo 2022) 
+IBGE
+Área: 495,39 km² 
+IBGE
+PIB em reais: R$ 81562848000 (2021) 
+ARQUIVO DO ESTADO DO RIO GRANDE DO SUL
+Número de pontos turísticos: 50
+
+Codigo da Carta:B02
+Nome da Cidade:Gramado
+População: 38.721 habitantes (estimativa 2023)
+Área: 237,019 km²
+PIB em reais: R$ 2,8 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 30 principais pontos turísticos
+
+Codigo da Carta:B03
+Nome da Cidade:caxias do sul 
+População: 525.746 habitantes (estimativa 2023)
+Área: 1.643,6 km²
+PIB em reais: R$ 29,4 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 20 principais pontos turísticos
+
+
+Codigo da Carta:B04
+Nome da Cidade:Passo Fundo
+População: 214.564 habitantes (estimativa IBGE 2024) 
+WIKIPEDIA
+Área: 783,421 km² 
+WIKIPEDIA
+PIB em reais: R$ 12,559 bilhões (IBGE 2021) 
+WIKIPEDIA
+Número de pontos turísticos:  17 pontos turísticos na cidade. 
+
+
+
+
+
+Estado: C 
+Codigo da Carta:C01
+Nome da Cidade: Curitiba
+População: 1948626 habitantes (estimativa 2023)
+Área: 435,036 km²
+PIB em reais: R$ 96,2 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 40 principais pontos turísticos
+
+Codigo da Carta:C02
+Nome da Cidade:Londrina
+População: 588.125 habitantes (estimativa 2023)
+Área: 1.650,809 km²
+PIB em reais: R$ 26,8 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 20 principais pontos turísticos
+
+
+
+
+
+Codigo da Carta:C03
+Nome da Cidade:Ponta Grossa
+População: 372.562 habitantes (estimativa 2024) 
+WIKIPEDIA
+Área: 2.054,732 km² 
+WIKIPEDIA
+PIB em reais: R$ 15,05 bilhões (2018) 
+WIKIPEDIA
+Número de pontos turísticos:15 pontos turisticos
+
+
+Codigo da Carta:C04
+Nome da Cidade:Cascavel
+População: 349.926 habitantes (estimativa 2023)
+Área: 2.100,083 km²
+PIB em reais: R$ 18,5 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 15 principais pontos turísticos
+
+
+Estado: D 
+Codigo da Carta:D01
+Nome da Cidade: Belo Horizonte 
+População: 2530701 habitantes (estimativa 2023)
+Área: 331,4 km²
+PIB em reais: R$ 101,1 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 50 principais pontos turísticos
+
+Codigo da Carta:D02
+Nome da Cidade:Lavras Novas
+
+
+
+
+
+Codigo da Carta:D03
+Nome da Cidade:Ouro Preto
+População: 74.874 habitantes (estimativa 2023)
+Área: 1.245,12 km²
+PIB em reais: R$ 2,7 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 30 principais pontos turísticos
+
+
+
+
+Codigo da Carta:D04
+Nome da Cidade:Governador Valadares
+População: 257.171 habitantes (Censo 2022) 
+IBGE
+Área: 2.342,376 km² 
+IBGE
+PIB em reais: R$ 6,72 bilhões (2020) 
+DATAVIVA
+Número de pontos turísticos: 20 atrações principais
+
+
+
+
+Estado :E 
+Codigo da Carta:E01
+Nome da Cidade: Salvador
+População: 2900319 habitantes (estimativa 2023)
+Área: 692,819 km²
+PIB em reais: R$ 64,1 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 30 principais pontos turísticos
+
+Codigo da Carta:E02
+Nome da Cidade:Ilheus
+População: 178.703 habitantes (Censo 2022) 
+G1
+Área: 1.588,555 km² 
+IBGE
+PIB em reais: R$ 5,8 bilhões (2020) 
+CIDADES IBGE
+Número de pontos turísticos: 25 pontos turisticos
+
+
+
+Codigo da Carta:E03
+Nome da Cidade:Feira de Santana
+População: 616.272 habitantes (Censo 2022) 
+WIKIPEDIA
+Área: 1.304,425 km² 
+WIKIPEDIA
+PIB em reais: R$ 17,28 bilhões (2021) 
+WIKIPEDIA
+Número de pontos turísticos:20 pontos turisticos
+
+Codigo da Carta:E04
+Nome da Cidade:Itacaré
+População: 27.704 habitantes (Censo 2022) 
+IBGE
+Área: 726,265 km² 
+IBGE
+PIB em reais: R$ 14.680,53 per capita (2021) 
+IBGE
+Número de pontos turísticos: 25
+
+
+
+Estado:F 
+Codigo da Carta:F01
+Nome da Cidade: Vitoria
+População: 369.534 habitantes (estimativa 2023)
+Área: 93,381 km²
+PIB em reais: R$ 35,5 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 20 principais pontos turísticos
+
+Codigo da Carta:F02
+Nome da Cidade:colatina
+População: 120.033 habitantes (Censo 2022) 
+IBGE
+Área: 1.398,219 km² 
+IBGE
+PIB em reais: R$ 4,4 bilhões (2021) 
+CARAVELA
+Número de pontos turísticos 15
+
+
+
+
+
+
+
+Codigo da Carta:F03
+Nome da Cidade:Guarapari
+População: 124.656 habitantes (Censo 2022) 
+G1
+Área: 589,826 km² 
+WIKIPEDIA
+PIB em reais: R$ 2,077 bilhões (2016) 
+WIKIPEDIA
+Número de pontos turísticos: 50
+
+
+
+
+
+
+Codigo da Carta:F04
+Nome da Cidade:cachoeiro de Itapemirim
+População: 185.784 habitantes (Censo 2022) 
+WIKIPEDIA
+Área: 864,583 km² 
+IBGE
+PIB em reais: R$ 4,837 bilhões (2018) 
+WIKIPEDIA
+Número de pontos turísticos:30
+
+
+Estado:G 
+Codigo da Carta:G01
+Nome da Cidade: Natal 
+População: 896.708 habitantes (estimativa 2023)
+Área: 167,3 km²
+PIB em reais: R$ 24,7 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 25 principais pontos turísticos
+
+Codigo da Carta:F02
+Nome da Cidade:Parnamirim
+População: 252.716 habitantes (Censo 2022) 
+G1
+Área: 123,471 km² 
+ROTEIROS INTELIGENTES
+PIB em reais: R$ 1,654 bilhões (dados de 2008) 
+ENCONTRA PARNAMIRIM
+Número de pontos turísticos:20
+
+
+Codigo da Carta:F03
+Nome da Cidade:mossoró
+População: 264.577 habitantes (Censo 2022) 
+G1
+Área: 2.099,33 km² 
+CIDADES SUSTENTÁVEIS
+PIB em reais: R$ 6,926 bilhões (2019) 
+WIKIPEDIA
+Número de pontos turísticos: 20
+
+
+
+
+Codigo da Carta:F04
+Nome da Cidade:São Miguel do Gostoso
+População: 10.221 habitantes (Censo 2022) 
+G1
+Área: 431,444 km² 
+IBGE
+PIB em reais: R$ 492,6 milhões (2021) 
+CARAVELA
+Número de pontos turísticos:10
+
+
+Estado :H 
+Codigo da Carta:H01
+Nome da Cidade: Manaus
+População: 2.255.903 habitantes (estimativa 2023)
+Área: 11.401 km²
+PIB em reais: R$ 114,8 bilhões (IBGE 2020)
+Número de pontos turísticos: Aproximadamente 30 principais pontos turísticos
+
+Codigo da Carta:H02
+Nome da Cidade:Parintins
+População: 96.372 habitantes (Censo 2022) 
+IBGE
+Área: 5.956,047 km² 
+IBGE
+PIB em reais: R$ 1,426 bilhões (2021) 
+CIDADES IBGE
+Número de pontos turísticos: 15
+
+
+Codigo da Carta:H03
+Nome da Cidade:Tefé
+População: 73.669 habitantes (Censo 2022) 
+G1
+Área: 23.808 km² 
+WIKIPEDIA
+PIB em reais: R$ 934.210.620,00 (2018) 
+WIKIPEDIA
+Número de pontos turísticos: 10
+
+Codigo da Carta:H04
+Nome da Cidade:Itacoatiara
+População: 103.598 habitantes (Censo 2022) 
+G1
+Área: 8.891,906 km² 
+WIKIPEDIA
+PIB em reais: R$ 2,05 bilhões (2016) 
+WIKIPEDIA
+Número de pontos turísticos:10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // Você pode utilizar o código do primeiro desafio
     printf("Desafio das Cartas!\n");
 
